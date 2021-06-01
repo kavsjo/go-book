4155876330a3e16568731161d3baded80d6cd788
kavsjo <kavsjo88@hotmail.se>
2021-06-01 13:02:29 -0700

    Second commit

--- linkgraph/store/memory/memory.go
+++ linkgraph/store/memory/memory.go
@@ -1,6 +1,9 @@
 package memory
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type edgeList []uuid.UUID
 
@@ -13,3 +16,64 @@ type InMemoryGraph struct {
 	linkURLIndex map[string]*graph.Link
 	linkEdgeMap  map[uuid.UUID]edgeList
 }
+
+func (s *InMemoryGraph) UpsertLink(link *graph.Link) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if link.ID = uuid.Nil {
+		link.ID = existing.ID
+		origTs := existing.RetrievedAt
+		*existing = *link
+		if origTs.After(existing.RetrievedAt) {
+			existing.RetrievedAt = origTs
+		}
+		return nil
+	}
+
+	for {
+		link.ID = uuid.New()
+		if s.links[link.ID] == nil {
+			break
+		}
+	}
+
+	lCopy := new(graph.Link)
+	*lCopy = *link
+	s.linkURLIndex[lCopy.URL] = lCopy
+	return nil
+}
+
+func (s *InMemoryGraph) UpsertEdge()  {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	_, srcExists := s.links[edge.Src]
+	_, dstExists := s.links[edge.Dst]
+	if !srcExists || !dstExists {
+		return xerrors.Errorf("upsert edge: %w", graph.ErrUnknownEdgeLink)
+	}
+
+	for _, edgeID := range s.linkEdgeMap[edge.Src] {
+		existingEdge := s.edges[edgeID]
+		if existingEdge.Src == edge.Src && existingEdge.Dst == edge.Dst {
+			existingEdge.UpdateAt = time.Now()
+			*edge = *existingEdge
+			return nil
+		}
+	}
+
+	for {
+		edge.ID = uuid.New()
+
+		if s.edges[edge.ID] == nil {
+			break
+		}
+	}
+
+	edge.UpdateAt = time.Now()
+	eCopy := new(graph.Edge)
+	*eCopy = *edge 
+	s.edges[eCopy.ID] = eCopy
+
+	s.linkEdgeMap[edge.Src] = append(s.linkEdgeMap[edge.Src], eCopy.ID)
